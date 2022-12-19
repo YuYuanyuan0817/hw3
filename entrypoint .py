9aedecdbcc35d389ecd6d199cf5f87ce221dce8f
YuYuanyuan0817 <118308478+YuYuanyuan0817@users.noreply.github.com>
2022-12-20 07:42:24 +0800

    change argparse function

--- entrypoint .py
+++ entrypoint .py
@@ -4,10 +4,10 @@ from model.svm import predict2
 
 
 #random forest model prediction
-logging.info (f"prediction:{predict1([[59, 1, 0, 101, 234, 0, 1, 143, 0, 3.4, 0, 0, 0]],'model1/conf/random_forest.pkl')}")
+logging.info (f"prediction:{predict1([[59, 1, 0, 101, 234, 0, 1, 143, 0, 3.4, 0, 0, 0]],'model/conf/randomforest.pkl')}")
 
 #svm model prediction
-logging.info (f"prediction:{predict2([[59, 1, 0, 101, 234, 0, 1, 143, 0, 3.4, 0, 0, 0]],'model1/conf/random_forest.pkl')}")
+logging.info (f"prediction:{predict2([[59, 1, 0, 101, 234, 0, 1, 143, 0, 3.4, 0, 0, 0]],'model/conf/svm.pkl')}")
 
 
 import argparse
@@ -15,8 +15,32 @@ import argparse
 def parse_args():
     parser=argparse.ArgumentParser()
     parser.add_argument("--prediction_model",type=str,help="model filename")
-    parser.add_argument("--prediction_params",type=int,help="params number")
+    parser.add_argument("--prediction_params",type=float,help="params number")
     return parser.parse_args()
 
 
 
+
+def create_parser():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--prediction_params",
+                        type=float,
+                        help="params number")
+
+    parser.add_argument("--prediction_model",
+                        type=str,
+                        dest="model_name", 
+                        choices=['randomforest','svm'],
+                        help="return the name of the model")
+    return parser
+
+if __name__ == "__main__":
+
+    parser = create_parser()
+   
+    args = parser.parse_args()
+    
+    print('args dict:', vars(args))
+    
+    
