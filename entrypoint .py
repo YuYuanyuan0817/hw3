d5f7d9cc0ae5b750ed7555103fbb93ba641d3433
YuYuanyuan0817 <118308478+YuYuanyuan0817@users.noreply.github.com>
2022-12-31 01:38:58 +0800

    entrypiont code

--- entrypoint .py
+++ entrypoint .py
@@ -1,13 +1,13 @@
-from model.randomforest  import predict1
-from conf.conf import logging
-from model.svm import predict2
+from model.randomforest  import predict
+from confv.conf import logging
+from model.svm import predict
 
 
 #random forest model prediction
-logging.info (f"prediction:{predict1([[59, 1, 0, 101, 234, 0, 1, 143, 0, 3.4, 0, 0, 0]],'model/conf/randomforest.pkl')}")
+logging.info (f"prediction:{predict([[59, 1, 0, 101, 234, 0, 1, 143, 0, 3.4, 0, 0, 0]],path_to_model ='model/conf/decision_tree.pkl')}")
 
 #svm model prediction
-logging.info (f"prediction:{predict2([[59, 1, 0, 101, 234, 0, 1, 143, 0, 3.4, 0, 0, 0]],'model/conf/svm.pkl')}")
+logging.info (f"prediction:{predict([[59, 1, 0, 101, 234, 0, 1, 143, 0, 3.4, 0, 0, 0]],path_to_model ='model/conf/decision_tree.pkl')}")
 
 
 import argparse
@@ -43,4 +43,4 @@ if __name__ == "__main__":
     
     print('args dict:', vars(args))
     
-    
+ 
