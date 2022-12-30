8f45c97a86ff936304613e34a688b63aaaddcc67
YuYuanyuan0817 <118308478+YuYuanyuan0817@users.noreply.github.com>
2022-12-31 01:38:04 +0800

    Modify the code and re-adjust the hyperparameters

--- randomforest.py
+++ randomforest.py
@@ -1,20 +1,17 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from connector.pg_connector import get_data
 from sklearn.model_selection import KFold, train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from  conf.conf import logging
+from  confv.conf import logging
 import pickle
 from util.util import save_model,load_model
-from conf.conf import settings
-
-logging.info("extracting df")
-
-# Load the dataset
-df = get_data('https://raw.githubusercontent.com/5x12/ml-cookbook/master/supplements/data/heart.csv')
-
-logging.info("DF is extracted")
+from confv.conf import settings
+from confv.conf import path_to_model
 
 
-def train_test_split(df):
+def split_data(df):
     logging.info("Select X and y")
     # Filter out target column and take all other columns
     X = df.iloc[:, :-1]
@@ -24,8 +21,7 @@ def train_test_split(df):
     
     logging.info("Split variables")
     # Split variables into train and test
-    X_train, X_test, y_train, y_test = train_test_split(X,y,random_state = 0)
-    
+    X_train, X_test, y_train, y_test = train_test_split(X,y)
     return X_train, X_test, y_train, y_test
 
 
@@ -38,47 +34,34 @@ def train_random_forest (X_train, y_train):
     logging.info("train model")
     # Train the model
     clf.fit(X_train, y_train)
-    
-    save_model(dir='model/conf/randomforest.pkl',model=clf)
-    
+    save_model(dir=path_to_model,model=clf) 
     return clf
 
-
-print(f"parameter{settings.DATA.data_set}")
-
-
-df = get_data('https://raw.githubusercontent.com/5x12/ml-cookbook/master/supplements/data/heart.csv')
-X_train, X_test, y_train, y_test = train_test_split(df)
+df = get_data(settings.DATA.data_set)
+X_train, X_test, y_train, y_test =split_data(df)
 clf = train_random_forest (X_train, y_train)
 logging.info(f'Accuracy is {clf.score(X_test,y_test)}')
 
 
+
 #Hyperparameters Tuning
-from sklearn.model_selection import GridSearchCV, cross_val_score,KFold
-clf_params = {'n_estimators':[100,300,500,700],"max_depth": range(1,12), 'max_features':[3,4,5],'min_samples_leaf':range(1,10)}
-clf_grid = GridSearchCV(clf, clf_params, cv=KFold(shuffle=True,random_state=0), n_jobs=-1)
-clf_grid.fit(X_train, y_train)
-clf_grid.best_params_, clf_grid.best_score_
-logging.info(f'clf_grid with best_params_ = {clf_params}')
-logging.info(f'Accuracy is {clf.score(X_train,y_train)}')
-logging.info(f'Accuracy is {clf.score(X_test,y_test)}')
 
-
+for depth in range(1,12):
+    for features in [3,4,5]:
+        clf =RandomForestClassifier(max_depth = depth,max_features= features)
+        clf.fit(X_train, y_train)
+        
+        logging.info(f'max_depth = {depth} & max_features = {features}')
+        logging.info(f'Accuracy is {clf.score(X_train,y_train)}')
+        logging.info(f'Accuracy is {clf.score(X_test,y_test)}')
 
 #Making a prediction
-def predict1 (values):
-    clf = load_model('model/conf/randomforest.pkl')
-    return clf. predict1(values)
+def predict (values,path_to_model):
+    clf = load_model(path_to_model)
+    return clf. predict(values)
 
-
-
-
-
-responce = predict1(X_test)
-clf = load_model('model/conf/randomforest.pkl')
-
-logging.info(f'Prediction is {clf.predict1(X_test)}')
-
+responce = predict(X_test,path_to_model)
+logging.info(f'Prediction is {clf.predict(X_test)}')
 
 
 
