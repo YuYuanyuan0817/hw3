1f6d0dca76705c05e95ef7f7fc868935f256a746
YuYuanyuan0817 <118308478+YuYuanyuan0817@users.noreply.github.com>
2022-12-31 01:37:27 +0800

    Modify the code and re-adjust the hyperparameters

--- svm.py
+++ svm.py
@@ -1,21 +1,17 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from connector.pg_connector import get_data
 from sklearn.svm import SVC
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from  conf.conf import logging
+from  confv.conf import logging
 import pickle
 from util.util import save_model,load_model
-from conf.conf import settings
+from confv.conf import settings
+from confv.conf import path_to_model
 
-logging.info("extracting df")
-
-# Load the dataset
-df = get_data('https://raw.githubusercontent.com/5x12/ml-cookbook/master/supplements/data/heart.csv')
-
-logging.info("DF is extracted")
-
-
-def train_test_split(df):
+def split_data(df):
     logging.info("Select X and y")
     # Filter out target column and take all other columns
     X = df.iloc[:, :-1]
@@ -25,7 +21,7 @@ def train_test_split(df):
     
     logging.info("Split variables")
     # Split variables into train and test
-    X_train, X_test, y_train, y_test = train_test_split(X,y,random_state = 3)
+    X_train, X_test, y_train, y_test = train_test_split(X,y)
     
     return X_train, X_test, y_train, y_test
 
@@ -35,26 +31,21 @@ def train_svc (X_train, y_train):
     logging.info("intialize model")
 
     # Initialize the model
-    clf =  SVC(random_state=3, probability=True)
-   
+    svm =  SVC(random_state=3, probability=True)
     logging.info("train model")
 
     # Train the model
-    clf.fit(X_train, y_train)
-    
-    save_model(dir='model/conf/svm.pkl',model=clf)
-    
-
-    return clf
+    svm.fit(X_train, y_train)
+    save_model(dir=path_to_model,model=svm) 
+    return svm
 
 
 
 
-df = get_data('https://raw.githubusercontent.com/5x12/ml-cookbook/master/supplements/data/heart.csv')
-X_train, X_test, y_train, y_test = train_test_split(df)
-clf = train_svc (X_train, y_train)
-
-logging.info(f'Accuracy is {clf.score(X_test,y_test)}')
+df = get_data(settings.DATA.data_set)
+X_train, X_test, y_train, y_test =split_data(df)
+svm = train_svc (X_train, y_train)
+logging.info(f'Accuracy is {svm.score(X_test,y_test)}')
 
 #Hyperparameters Tuning
 
@@ -63,26 +54,20 @@ for this_gamma in [0.001, 0.01, 0.1]:
     for this_C in [0.1, 1, 15]:
         
         #initialize a model
-        clf = SVC(gamma=this_gamma, C = this_C)
+        svm = SVC(gamma=this_gamma, C = this_C)
         
         #fit the model
-        clf.fit(X_train, y_train)
+        svm.fit(X_train, y_train)
+        
         logging.info(f'SVM with gamma = {this_gamma} & C = {this_C}')
-        logging.info(f'Accuracy is {clf.score(X_train,y_train)}')
-        logging.info(f'Accuracy is {clf.score(X_test,y_test)}')
+        logging.info(f'Accuracy is {svm.score(X_train,y_train)}')
+        logging.info(f'Accuracy is {svm.score(X_test,y_test)}')
 
 
 #Making a prediction
+def predict (values,path_to_model):
+    svm = load_model(path_to_model)
+    return svm.predict(values)
 
-def predict2 (values):
-    clf = load_model('model/conf/svm.pkl')
-    return clf.predict2(values)
-
-
-
-
-responce = predict2(X_test)
-
-clf = load_model('model/conf/svm.pkl')
-
-logging.info(f'Prediction is {clf.predict2(X_test)}')
+responce = predict(X_test,path_to_model)
+logging.info(f'Prediction is {svm.predict(X_test)}')
